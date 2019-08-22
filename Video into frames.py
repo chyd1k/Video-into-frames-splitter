fddfb7675d86262e2a876e71f5f5bbb35e36d430
Bogdan <51471521+chyd1k@users.noreply.github.com>
2019-08-22 22:36:37 +0300

    Add files via upload

---
+++ Video into frames.py
@@ -0,0 +1,25 @@
+import cv2
+
+def vid_into_frames(capture, every_x_frames):
+    count_frames, frame_number, success = 0, 0, True
+    while 1:
+        success, image = capture.read()
+        if success == True:
+            if every_x_frames == 0 or count_frames % every_x_frames == 0:
+                cv2.imwrite(f'frame{frame_number}.jpg', image)
+                print(f'Successfully written {frame_number} frame')
+                count_frames = 0
+                frame_number += 1
+            count_frames += 1
+        else:
+            exit()
+
+def main():
+    capture = cv2.VideoCapture('69989717_1331325190377077_6883900595897995615_n.mp4')
+    # A value that determines how often the frame from the video will be saved.
+    # Place 0 - if you want to get every frame from the video.
+    every_x_frames = 10
+    vid_into_frames(capture, every_x_frames)
+
+if __name__ == '__main__':
+    main()
